e5a1024f43ca2e3aec1d14874e8ae8ddf1a23744
lxclxclxclxc <50006993+lxclxclxclxc@users.noreply.github.com>
2019-04-26 12:37:58 +0800

    Update Data.java

--- Data.java
+++ Data.java
@@ -12,4 +12,14 @@ public class Data {
 	public  int[][] LT = {{0,14,11,0},
 			              {14,23,27,11}};
 	
+        public  int[][] MT = {{0,14},
+			      {14,23}};
+	
+	public  int[][] ST = {{14,11}};
+	
+	public  int[][] SQ = {{14,11},
+			      {12,13}};
+
+	public  int[][] PA = {{14,23,13}};
+	
 }
